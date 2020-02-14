ef868e6b9431e9ae62fc12697376af6db4392337
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-14 01:03:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
  i = name_hash.length
   while name_hash.length > 1 do
-    name_hash.max_by {|k,v| name_hash.delete!(:k)}
+    name_hash.max_by {|k,v| name_hash.delete(:k)}
     i -= 1
   end
   name_hash
