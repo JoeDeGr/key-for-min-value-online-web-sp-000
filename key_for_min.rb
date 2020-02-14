bf2d764ed7c19f30ac3314d75ef9d9b8cc875d81
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-14 01:01:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ i = name_hash.length
+  while name_hash.length > 1 do
+    name_hash.max_by {|k,v| name_hash.delete!(k)}
+    end
+    i -= 1
+  end
+  name_hash
 end
